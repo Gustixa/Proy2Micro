f9a1f17e7ce5c2102839da098f61b5a21bff142e
Samuel Argueta <arg211024@uvg.edu.gt>
2022-10-07 08:29:49 -0600

    Carga de metodos

--- Code.cpp
+++ Code.cpp
@@ -15,7 +15,39 @@
 #include <unistd.h>
 #include <time.h>
 
+pthread_mutex_t mutexCar;
+int none;
+
+// Prototypes
+void *gasStation(void *argument);
+void *gasPrice(void *argument);
+void *fillGas(void *argument);
+
+/**
+ * @brief
+ *
+ * @param argc
+ * @param argv
+ * @return int
+ */
 int main(int argc, char *argv[])
 {
+    int amountGasStation;
+    pthread_t numGasStation[amountGasStation];
+    pthead_mutex_init(&mutexCar);
+
+    pthread_mutex_destroy(&mutexCar);
     return 0;
+}
+
+void *gasStation(void *argument)
+{
+}
+
+void *gasPrice(void *argument)
+{
+}
+
+void *fillGas(void *argument)
+{
 }
