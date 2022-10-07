d590386fdfe36f64d4b057eabe98410caeb5cc6b
Samuel Argueta <arg211024@uvg.edu.gt>
2022-10-07 07:31:10 -0600

    Carga inicial del proyecto

---
+++ Code.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file Code.cpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-10-02
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
